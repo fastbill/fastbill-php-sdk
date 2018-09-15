58c8c5b7dde7a4a181087f74d6ceeef424ccc562
Thomas Eiling <teiling88@gmail.com>
2018-09-15 23:39:32 +0200

    adding remaining entities

---
+++ src/Items/ItemsEntity.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace FastBillSdk\Items;
+
+class ItemsEntity
+{
+    const FIELD_MAPPING = [];
+
+    public function __construct(\SimpleXMLElement $data = null)
+    {
+        if ($data) {
+            $this->setData($data);
+        }
+    }
+
+    /**
+     * @param \SimpleXMLElement $data
+     * @return ItemsEntity
+     */
+    public function setData(\SimpleXMLElement $data): self
+    {
+        foreach ($data as $key => $value) {
+            if (!isset(self::FIELD_MAPPING[$key])) {
+                trigger_error('the provided xml key ' . $key . ' is not mapped at the moment in ' . self::class);
+                continue;
+            }
+
+            $this->{self::FIELD_MAPPING[$key]} = (string) $value;
+        }
+
+        return $this;
+    }
+}
