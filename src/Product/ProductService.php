82965ab94086867424ae698196da6c1e72fa872e
Thomas Eiling <teiling88@gmail.com>
2019-10-17 10:09:29 +0200

    add get products example and implementation

--- src/Product/ProductService.php
+++ src/Product/ProductService.php
@@ -2,6 +2,52 @@
 
 namespace FastBillSdk\Product;
 
+use FastBillSdk\Api\ApiClientInterface;
+use FastBillSdk\Common\XmlService;
+
 class ProductService
 {
+    /**
+     * @var ApiClientInterface
+     */
+    private $apiClient;
+
+    /**
+     * @var XmlService
+     */
+    private $xmlService;
+
+    /**
+     * @var ProductValidator
+     */
+    private $validator;
+
+    public function __construct(ApiClientInterface $apiClient, XmlService $xmlService, ProductValidator $validator)
+    {
+        $this->apiClient = $apiClient;
+        $this->xmlService = $xmlService;
+        $this->validator = $validator;
+    }
+
+    /**
+     * @return ProductEntity[]
+     */
+    public function getProducts(ProductSearchStruct $searchStruct): array
+    {
+        $this->xmlService->setService('article.get');
+        $this->xmlService->setFilters($searchStruct->getFilters());
+        $this->xmlService->setLimit($searchStruct->getLimit());
+        $this->xmlService->setOffset($searchStruct->getOffset());
+
+        $response = $this->apiClient->post($this->xmlService->getXml());
+
+        $xml = new \SimpleXMLElement((string) $response->getBody());
+
+        $results = [];
+        foreach ($xml->RESPONSE->ARTICLES->ARTICLE as $articleEntity) {
+            $results[] = new ProductEntity($articleEntity);
+        }
+
+        return $results;
+    }
 }
