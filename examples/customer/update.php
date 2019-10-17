7fe6d54b8ea9793d339f21eade2ce1a8862a95bf
Thomas Eiling <teiling88@gmail.com>
2019-10-17 10:33:58 +0200

    adding more examples for customer namespace

---
+++ examples/customer/update.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+require_once __DIR__ . '/../credentials.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$fastBillClient = new FastBillSdk\Api\ApiClient($username, $apiKey);
+
+$customersService = new FastBillSdk\Customer\CustomerService(
+    $fastBillClient,
+    new \FastBillSdk\Common\XmlService(),
+    new \FastBillSdk\Customer\CustomerValidator()
+);
+
+$customersEntity = new \FastBillSdk\Customer\CustomerEntity();
+$customersEntity->customerType = \FastBillSdk\Customer\CustomerEntity::CUSTOMER_TYPE_CONSUMER;
+$customersEntity->customerId = 1111;
+$customersEntity->firstName = 'FirstName v2';
+$customersEntity->lastName = 'LastName v2';
+$result = $customersService->updateCustomer($customersEntity);
+
+ini_set('xdebug.var_display_max_depth', '5');
+ini_set('xdebug.var_display_max_children', '256');
+ini_set('xdebug.var_display_max_data', '1024');
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
+die();
